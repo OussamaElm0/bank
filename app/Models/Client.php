a912ff0bcb571387a4cc11cdd49c809e8df18cde
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-05-09 14:30:17 +0100

    Create client and virement models and migrations

---
+++ app/Models/Client.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    protected $table = 'clients';
+    protected $fillable = [
+        'user_id',
+        'solde'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function virements()
+    {
+        return $this->hasMany(Virement::class);
+    }
+}
