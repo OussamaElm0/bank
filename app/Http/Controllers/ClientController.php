256c5f8a409681e8741dce014ab9d5b311c256af
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-05-09 15:32:09 +0100

    Create client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Client;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 
 class ClientController extends Controller
 {
@@ -31,7 +34,24 @@ class ClientController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Client::create([
+            'user_id' => $user->id,
+            'solde' => $request->solde,
+        ]);
+
+        return redirect()->route('clients.index');
     }
 
     /**
--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -1,17 +1,54 @@
 <x-guest-layout>
-    <form method="post" action="{{ route('types.store') }}" enctype="multipart/form-data">
+    <form method="POST" action="{{ route('clients.store') }}">
         @csrf
 
-        <!-- type Name -->
+        <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('type Name')" />
+            <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Solde -->
+        <div>
+            <x-input-label for="solde" :value="__('Solde')" />
+            <x-text-input id="solde" class="block mt-1 w-full" type="number" min="0" name="solde" :value="old('solde')" required autofocus />
+            <x-input-error :messages="$errors->get('solde')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                          type="password"
+                          name="password"
+                          required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                          type="password"
+                          name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <x-primary-button class="ms-4">
-                {{ __('Add type') }}
+                {{ __('Creer') }}
             </x-primary-button>
         </div>
     </form>
